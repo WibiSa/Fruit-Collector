8bed20d3a78283299ac9e3a4fe73f17cac00363b
wibisa <rwibisa@gmail.com>
2022-09-24 12:12:20 +0800

    membuat splash screen

--- app/src/main/java/com/wibisa/fruitcollector/ui/MainActivity.kt
+++ app/src/main/java/com/wibisa/fruitcollector/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.wibisa.fruitcollector.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.wibisa.fruitcollector.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
